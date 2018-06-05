125672ea008642751ba68cd7eb50280ed8b6ea7f
Aman Garg <aman.garg@accoliteindia.com>
2018-06-05 14:36:42 +0530

    Initial script setup

---
+++ prod-monitor.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Shell script to monitor production server logs and store the result in a file
+# Author: Aman Garg
+# First release: 05/06/2018
+# Usage:
+#  $ ./prod-monitor.sh param1 param2
+# * param1: url of the prod server which is to be monitored
+# * param2: absolute path of the log file used to generate results
+echo $0 
+echo $1 
+echo $2 
